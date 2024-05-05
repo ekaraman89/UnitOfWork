7e0fdb6f6b0d4ff9e59d8c7ef096ad25ac804922
emrah karaman <ekaraman89@hotmail.com>
2024-05-05 22:33:21 +0300

    unit of work

--- UnitOfWork/Program.cs
+++ UnitOfWork/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
@@ -16,29 +23,112 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", ([FromServices] IUnitOfWork unitOfWork) =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
+        var b = unitOfWork.ProductRepository;
+        unitOfWork.SaveChanges();
     })
     .WithName("GetWeatherForecast")
     .WithOpenApi();
 
+app.MapGet("/weatherforecast2", ([FromServices] IUnitOfWork unitOfWork) =>
+    {
+        var b = unitOfWork.CustomerRepository;
+        unitOfWork.SaveChanges();
+    })
+    .WithName("GetWeatherForecast2")
+    .WithOpenApi();
+
+app.MapGet("/weatherforecast3", ([FromServices] IUnitOfWork unitOfWork) =>
+    {
+        var b = unitOfWork.ProductRepository;
+        var c = unitOfWork.CustomerRepository;
+        unitOfWork.SaveChanges();
+    })
+    .WithName("GetWeatherForecast33")
+    .WithOpenApi();
+
+
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+
+public class UnitOfWork(IServiceProvider serviceProvider) : IUnitOfWork
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    private IProductRepository? _productRepository;
+    private ICustomerRepository? _customerRepository;
+
+    public IProductRepository ProductRepository
+    {
+        get
+        {
+            if (_productRepository == null)
+            {
+                _productRepository = serviceProvider.GetRequiredService<IProductRepository>();
+            }
+
+            return _productRepository;
+        }
+    }
+
+    public ICustomerRepository CustomerRepository
+    {
+        get
+        {
+            if (_customerRepository == null)
+            {
+                _customerRepository = serviceProvider.GetRequiredService<ICustomerRepository>();
+            }
+
+            return _customerRepository;
+        }
+    }
+
+    public void SaveChanges()
+    {
+        Console.WriteLine("Saving changes");
+    }
+}
+
+public interface IProductRepository
+{
+    void Hello();
+}
+
+public class ProductRepository : IProductRepository
+{
+    public ProductRepository()
+    {
+        Console.WriteLine("Product repository created");
+    }
+
+    public void Hello()
+    {
+        Console.WriteLine("Hello from product repository");
+    }
+}
+
+public interface ICustomerRepository
+{
+    void Hello();
+}
+
+public class CustomerRepository : ICustomerRepository
+{
+    public CustomerRepository()
+    {
+        Console.WriteLine("Customer repository created");
+    }
+
+    public void Hello()
+    {
+        Console.WriteLine("Hello from customer repository");
+    }
+}
+
+public interface IUnitOfWork
+{
+    IProductRepository ProductRepository { get; }
+    ICustomerRepository CustomerRepository { get; }
+    void SaveChanges();
 }
